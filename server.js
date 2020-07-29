089d98c5c935174339b362ca07f8399bea6e09cf
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-07-28 21:37:04 -0500

    add flavors html

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require('express')
+const exphbs = require('express-handlebars')
 
-var app = express();
+const app = express()
+const PORT = 7060
 
-var PORT = process.env.PORT || 7090;
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 var icecreams = [
-    {name: 'vanilla', price: 10, awesomeness: 3},
-    {name: 'chocolate', price: 4, awesomeness: 8},
-    {name: 'banana', price: 1, awesomeness: 1},
-    {name: 'greentea', price: 5, awesomeness: 7},
-    {name: 'jawbreakers', price: 6, awesomeness: 2},
-    {name: "pistachio", price: 11, awesomeness: 15 }
-  ];
+  {name: 'vanilla', price: 10, awesomeness: 3},
+  {name: 'chocolate', price: 4, awesomeness: 8},
+  {name: 'banana', price: 1, awesomeness: 1},
+  {name: 'greentea', price: 5, awesomeness: 7},
+  {name: 'jawbreakers', price: 6, awesomeness: 2},
+  {name: "pistachio", price: 11, awesomeness: 15 }
+];
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get(`/`, (req, res) => {
+  res.render('index')
 })
 
-app.get("/icecreams/:name", (req, res) => {
-    const targetFlavor = req.param.name
-    const found = icecreams.find(flavor => flavor.name === targetFlavor)
-    res.render("flavor", found)
-});
-
-app.get ("/icecreams", function (req, res){
-    res.render("icecreams")
+app.get(`/icecreams/:name`, (req, res) => {
+  const targetFlavor = req.params.name
+  const found = icecreams.find(flavor => flavor.name === targetFlavor)
+  res.render('flavor', found)
 })
 
+app.get(`/icecreams`, (req, res) => {
+  // loop over all the ice creams and display them all to the user.
+  res.render('flavors', { icecreams })
+})
 
-
-
-
-
-
-
-  app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, () => console.log(`🚀 on http://localhost:${PORT}`))
